2794130e5240e09c08dc4dc7d58f2d1352be6371
Armando <jose_netopio_12@hotmail.com>
2022-04-11 02:28:07 -0300

    reactjs-discover completo

---
+++ reactjs-discover/reactapp/src/pages/Home/index.jsx
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from 'react'
+import './styles.css'
+
+import { Card } from '../../Components/Card'
+
+export function Home() {
+
+  const [studentName, setStudentName] = useState('');
+  const [students, setStudents] = useState([]);
+  const [user, setUser] = useState({name:'', avatar: ''})
+
+  function handleAddStudent(){
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleString("pt-br", {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+      })
+    };
+
+    setStudents(prevState =>[...prevState, newStudent])
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+   const reposonse = await fetch("https://api.github.com/users/rodrigorgtic")
+   const data = await reposonse.json()
+   
+      setUser({
+        name: data.name,
+        avatar: data.avatar_url,
+      })
+    }
+
+    fetchData()
+  }, [])
+ 
+  return (
+    <div className="container">
+      <header>
+        <h1>Lista de Presença</h1>
+
+      <div>
+      </div>
+      <div>
+        <strong>{user.name}</strong>
+        <img src={user.avatar} alt="Foto de perfil" />
+      </div>
+
+      </header>
+
+      <input 
+        type="text"
+        placeholder="Digite o nome..." 
+        onChange={e => setStudentName(e.target.value)}
+      />
+
+      <button type="button" onClick={handleAddStudent}>
+        Adicionar
+      </button>
+
+      {
+        students.map(student => (
+         <Card
+          key={student.time}
+          name={student.name}
+          time={student.time} 
+         />
+        ))  
+      }
+     
+
+    </div>
+    
+  )
+}
+
